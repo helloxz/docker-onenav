c24c7bfe600613aa43bde7d14d5a4f0d3677619b
xiaoz <xiaoz93@outlook.com>
2021-03-22 17:28:31 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ onenav_path='/data/wwwroot/default'
 if [ ! -f "${onenav_path}/data/config.php" ]
 	then
 		cp ${onenav_path}/config.simple.php ${onenav_path}/data/config.php
+		cp ${onenav_path}/db/onenav.simple.db3 ${onenav_path}/data/onenav.db3
+		cp -r ${onenav_path}/db/index.html ${onenav_path}/db/
 		cd ${onenav_path}
 		sed -i "s%xiaoz'%${USER}'%g" data/config.php
 		sed -i "s%xiaoz.me'%${PASSWORD}'%g" data/config.php
