0c4386347e29161bd3b086852730f1727cdfe910
xiaoz <xiaoz93@outlook.com>
2021-03-22 16:45:27 +0800

    add

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+#更新软件
+apk update
+#安装timezone
+apk add -U tzdata
+#查看时区列表
+ls /usr/share/zoneinfo
+#拷贝需要的时区文件到localtime
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+#查看当前时间
+date
+#为了精简镜像，可以将tzdata删除了
+apk del tzdata
+#安装unzip
+apk add unzip
+#安装nginx
+apk add nginx
+#安装PHP
+apk add php7 php7-fpm php7-openssl php7-sqlite3 php7-curl php7-json php7-session
+#安装git
+apk add git
+
+#更名php-fpm配置文件
+mv /etc/php7/php-fpm.d/www.conf /etc/php7/php-fpm.d/www.conf.bak
+#更名PHP配置文件
+mv /etc/php7/php.ini /etc/php7/php.ini.bak
+#更名nginx配置文件
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.bak
+
+#复制配置文件
+cp /root/site.conf /etc/nginx/conf.d/
+cp /root/www.conf /etc/php7/php-fpm.d/
+cp /root/nginx.conf /etc/nginx/
+cp /root/run.sh /usr/sbin/
+#cp /root/php.ini /etc/php7/
+#启动脚本赋予权限
+chmod +x /usr/sbin/run.sh
+
+#创建目录
+mkdir -p /data/wwwroot/default
+#创建用户和用户组
+#addgroup -S www && adduser -S www -G www
+#chown -R www:www /data/wwwroot/default/
+#拉取代码
+cd 
+git /data/wwwroot/default
+#git glone https://github.com/helloxz/onenav.git .
+git clone -b dev https://github.com/helloxz/onenav.git .
+
+#替换路径
+#sed -i "s%\"thedir.*%\"thedir\"=>'/data/wwwroot/default',%g" zdir/config.php
+
+echo '----------------------------------------------'
+echo 'Install Success.'
+echo '----------------------------------------------'
---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+##########		name:Onenav容器运行入口			##########
+##########		update:2021/03/22				##########
+##########		author:xiaoz<xiaoz93@outlook.com>	######
+
+onenav_path='/data/wwwroot/default'
+
+#复制onenav配置
+if [ ! -f "${onenav_path}/data/config.php" ]
+	then
+		cp ${onenav_path}/config.simple.php ${onenav_path}/data/config.php
+		cd ${onenav_path}
+		sed -i "s%xiaoz'%${USER}'%g" data/config.php
+		sed -i "s%xiaoz.me'%${PASSWORD}'%g" data/config.php
+		#start php-fpm
+		php-fpm7 -D
+		#start nginx
+		/usr/sbin/nginx
+		tail -f /var/log/nginx/error.log
+	else
+		#start php-fpm
+		php-fpm7 -D
+		#start nginx
+		/usr/sbin/nginx
+		tail -f /var/log/nginx/error.log
+fi
