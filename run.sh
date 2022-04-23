1f4c1e7e2ec0d87f69eeb3ccafc28462aa97edc8
XiupingZou <xiaoz93@outlook.com>
2022-04-23 19:18:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,24 +5,8 @@
 
 onenav_path='/data/wwwroot/default'
 
-#复制onenav配置
-if [ ! -f "${onenav_path}/data/config.php" ]
-	then
-		cp ${onenav_path}/config.simple.php ${onenav_path}/data/config.php
-		cp ${onenav_path}/db/onenav.simple.db3 ${onenav_path}/data/onenav.db3
-		cp -r ${onenav_path}/db/index.html ${onenav_path}/data/
-		cd ${onenav_path}
-		sed -i "s%xiaoz'%${USER}'%g" data/config.php
-		sed -i "s%xiaoz.me'%${PASSWORD}'%g" data/config.php
-		#start php-fpm
-		php-fpm7 -RD
-		#start nginx
-		/usr/sbin/nginx
-		tail -f /var/log/nginx/error.log
-	else
-		#start php-fpm
-		php-fpm7 -RD
-		#start nginx
-		/usr/sbin/nginx
-		tail -f /var/log/nginx/error.log
-fi
+#start php-fpm
+php-fpm7 -RD
+#start nginx
+/usr/sbin/nginx
+tail -f /var/log/nginx/error.log
