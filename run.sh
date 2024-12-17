76cb7a99e4d1199b0d212834cc12d5c035f97d22
XIUPING ZOU <xiaoz93@outlook.com>
2024-12-17 12:47:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@
 
 onenav_path='/data/wwwroot/default'
 
+# Check if favicon.ico exists, if not, copy it
+if [ ! -f "$onenav_path/data/favicon.ico" ]; then
+    cp "$onenav_path/favicon.ico" "$onenav_path/data/favicon.ico"
+fi
+
 #start php-fpm
 php-fpm7 -RD
 #start nginx
