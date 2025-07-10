8165c39fa20733982ef29ec38f217780cf2ba99c
XIUPING ZOU <xiaoz93@outlook.com>
2025-07-10 15:05:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ apk del tzdata
 #安装nginx
 apk add nginx
 #安装PHP
-apk add php7 php7-fpm php7-openssl php7-sqlite3 php7-pdo_sqlite php7-curl php7-json php7-session php7-phar php7-iconv php7-mbstring php7-xml
+apk add php7 php7-fpm php7-openssl php7-sqlite3 php7-pdo_sqlite php7-curl php7-json php7-session php7-phar php7-iconv php7-mbstring php7-xml php7-dom
 #安装git
 apk add git
 
